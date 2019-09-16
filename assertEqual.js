872a42d18b2af08d5ee9576b0c585df17c73e59a
olicarignan <olive.carignan@gmail.com>
2019-09-16 18:48:53 +0000

    change for template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('👌👌👌Assertion Passed: ' + [actual] + ' === ' + [expected]);
+    console.log(`👌👌👌Assertion Passed:  ${actual} ===  ${expected}`);
   } else {
-    console.log('🚨🚨🚨Assertion Failed: ' + [actual] + ' !== ' + [expected]);
+    console.log(`🚨🚨🚨Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
