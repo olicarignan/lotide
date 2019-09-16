051d993e2ffc3e75c8f5603c84d345256ed59310
olicarignan <olive.carignan@gmail.com>
2019-09-16 18:56:06 +0000

    create head function

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👌👌👌Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨🚨🚨Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  let x =array[0];
+  return x;
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
