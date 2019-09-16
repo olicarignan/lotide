a8d994673dd210a101cac7ae4b2d51b4d9b6083b
olicarignan <olive.carignan@gmail.com>
2019-09-16 18:41:39 +0000

    assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('👌👌👌Assertion Passed: ' + [actual] + ' === ' + [expected]);
+  } else {
+    console.log('🚨🚨🚨Assertion Failed: ' + [actual] + ' !== ' + [expected]);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
