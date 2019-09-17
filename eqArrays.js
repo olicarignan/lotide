bc75c6fa05ac1c70f12311ff00e7435fde1edb98
olicarignan <olive.carignan@gmail.com>
2019-09-17 22:33:17 +0000

    create a function that compares if the elements of an array are equal

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌👌👌Assertion Passed:  ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🚨🚨🚨Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (arrayOne, arrayTwo) {
   let isTrue;
   for (let i = 0; i < arrayOne.length; i++) {
