95785ee61d03980e2b811c79519d151cf6110e40
olicarignan <olive.carignan@gmail.com>
2019-09-23 15:49:42 +0000

    dry up code

--- head.js
+++ head.js
@@ -1,15 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌👌👌Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`🚨🚨🚨Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   let x = array[0];
   return x;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
