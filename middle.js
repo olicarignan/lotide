33c730e7a5a4f7a55adda5b1095fc6d811ca256f
olicarignan <olive.carignan@gmail.com>
2019-09-23 17:21:57 +0000

    dry up code

--- middle.js
+++ middle.js
@@ -1,17 +1,13 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-
 
 const middle = function (input) {
   let midIndex;
-  if (input.length >= 1) {
+  if (input.length <= 1) {
     midIndex = [];
   } else if (input.length % 2 === 0) {
-    midIndex = '[' + Math.floor(input.length / 2) + ', ' + Math.floor(input.length / 2 + 1) + ']';
+    midIndex =  (Math.floor(input.length / 2) + Math.floor(input.length / 2 + 1));
   } else if (input.length % 2 !== 0) {
-    midIndex = '[' + Math.floor(input.length / 2 + 1) + ']';
+    midIndex = (Math.floor(input.length / 2 + 1));
   }
   return midIndex;
 }
-
 module.exports = middle;
