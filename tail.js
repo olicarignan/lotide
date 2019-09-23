2c236540020192462370f687a62ffee57cd2cb4f
olicarignan <olive.carignan@gmail.com>
2019-09-23 15:57:21 +0000

    dry up code

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌👌👌Assertion Passed:  ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🚨🚨🚨Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(input) {
   let output = [];
   for (let i = 1; i < input.length; i++) {
@@ -14,6 +6,4 @@ const tail = function(input) {
   return output;
 };
 
-let result = tail([1, 2, 3, 4, 5]);
-
-assertEqual(result.length, 4);
+module.exports = tail;
